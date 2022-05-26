707aa22945edd60c57a83466283e96c3547456d3
juledibishi <juled.ibishi@hotmail.com>
2022-05-26 23:20:31 +0200

    bug fixed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,19 +6,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { RestaurantComponent } from './restaurant/restaurant.component';
-import { CalculatorComponent } from './calculator/calculator.component';
-import { PersonatComponent } from './personat/personat.component';
-import { UshtrimeComponent } from './ushtrime/ushtrime.component';
-import { StudentetComponent } from './studentet/studentet.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    RestaurantComponent,
-    CalculatorComponent,
-    PersonatComponent,
-    UshtrimeComponent,
-    StudentetComponent
+    RestaurantComponent
   ],
   imports: [
     BrowserModule,
